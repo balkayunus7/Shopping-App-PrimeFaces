12ffd59ef925e7728111801e2368c11c896638bd
YUNUS BALKA <98759759+balkayunus7@users.noreply.github.com>
2024-01-25 19:35:47 +0300

    last details

---
+++ Demo/src/java/Models/OrderModel.java
@@ -0,0 +1,52 @@
+package Models;
+
+import java.sql.Timestamp;
+
+public class OrderModel {
+
+    private int id;
+    private String username;
+    private Timestamp createdAt;
+    private double total_price;
+    private int number;
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public double getTotal_price() {
+        return total_price;
+    }
+
+    public void setTotal_price(double total_price) {
+        this.total_price = total_price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+}
